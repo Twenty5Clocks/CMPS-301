5a7584f57520a1dc5dd97c01d7e97a2f35099d6a
Jeremy Driesler <JTDriesler@gmail.com>
2014-10-31 12:57:07 -0700

    more tesdting yep

--- helloWorld1/helloWorld1/Hello.cpp
+++ helloWorld1/helloWorld1/Hello.cpp
@@ -8,6 +8,8 @@ int main()
 	cout << "Hello World! This is the test of all tests!!!" << endl;
 	cout << "More testing yay!!" << endl;
 	
+	cout << "Even more testing YAY!!!!" << endl;
+	
 	system("Pause");
 	return 0;
 }
